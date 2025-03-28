197011d5a8755a9d728215c7f8a5bfa0d7268386
Matteo Vergnano <matteovergnano@MacBook-Pro-di-Matteo.local>
2025-03-28 10:31:36 +0100

    Aggiornato main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,8 @@ int main(void)
     using comp = complex_number<double>;
     double a = 4.454;
     comp b(6,3);
-    comp c1(1);
     comp c2(4,-2);
     comp c3;
-    cout << 4.454 + b << endl;
+    cout << a + b << endl;
     return 0;
 }
